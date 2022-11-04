5a7dc3440dbc4e5e4dc97c3ffadec1840576d6ac
Pieter Claerhout <pieter@contractify.be>
2022-11-04 16:37:59 +0100

    Code cleanup

--- komoot/responses.go
+++ komoot/responses.go
@@ -163,42 +163,44 @@ type UploadTourResponse struct {
 	} `json:"_links"`
 	Embedded struct {
 		Items   []UploadedTour `json:"items"`
-		Matched struct {
-			Constitution  int64      `json:"constitution"`
-			Status        string     `json:"status"`
-			Date          string     `json:"date"`
-			Difficulty    Difficulty `json:"difficulty"`
-			Distance      float64    `json:"distance"`
-			Duration      float64    `json:"duration"`
-			ElevationDown float64    `json:"elevation_down"`
-			ElevationUp   float64    `json:"elevation_up"`
-			Name          string     `json:"name"`
-			Path          []Path     `json:"path"`
-			Query         string     `json:"query"`
-			Segments      []Segment  `json:"segments"`
-			Source        string     `json:"source"`
-			Sport         string     `json:"sport"`
-			Summary       struct {
-				Surfaces []Surface `json:"surfaces"`
-				WayTypes []WayType `json:"way_types"`
-			} `json:"summary"`
-			TourInformation []TourInformation `json:"tour_information"`
-			Type            string            `json:"type"`
-			Embedded        struct {
-				Coordinates struct {
-					Items []Coordinate `json:"items"`
-				} `json:"coordinates"`
-				Directions struct {
-					Items []Direction `json:"items"`
-				} `json:"directions"`
-				Surfaces struct {
-					Items []EmbeddedSurface `json:"items"`
-				} `json:"surfaces"`
-				WayTypes struct {
-					Items []EmbeddedWayType `json:"items"`
-				} `json:"way_types"`
-			} `json:"_embedded"`
-		} `json:"matched"`
+		Matched MatchedTour    `json:"matched"`
 	} `json:"_embedded"`
 	Message string `json:"message"`
+}
+
+type MatchedTour struct {
+	Constitution  int64      `json:"constitution"`
+	Status        string     `json:"status"`
+	Date          string     `json:"date"`
+	Difficulty    Difficulty `json:"difficulty"`
+	Distance      float64    `json:"distance"`
+	Duration      float64    `json:"duration"`
+	ElevationDown float64    `json:"elevation_down"`
+	ElevationUp   float64    `json:"elevation_up"`
+	Name          string     `json:"name"`
+	Path          []Path     `json:"path"`
+	Query         string     `json:"query"`
+	Segments      []Segment  `json:"segments"`
+	Source        string     `json:"source"`
+	Sport         string     `json:"sport"`
+	Summary       struct {
+		Surfaces []Surface `json:"surfaces"`
+		WayTypes []WayType `json:"way_types"`
+	} `json:"summary"`
+	TourInformation []TourInformation `json:"tour_information"`
+	Type            string            `json:"type"`
+	Embedded        struct {
+		Coordinates struct {
+			Items []Coordinate `json:"items"`
+		} `json:"coordinates"`
+		Directions struct {
+			Items []Direction `json:"items"`
+		} `json:"directions"`
+		Surfaces struct {
+			Items []EmbeddedSurface `json:"items"`
+		} `json:"surfaces"`
+		WayTypes struct {
+			Items []EmbeddedWayType `json:"items"`
+		} `json:"way_types"`
+	} `json:"_embedded"`
 }
--- komoot/upload.go
+++ komoot/upload.go
@@ -11,6 +11,22 @@ import (
 )
 
 func (client *Client) Upload(name string, gpxData string, sport string) error {
+	importedGpx, err := client.importGpx(gpxData)
+	if err != nil {
+		return err
+	}
+
+	tour, err := client.importTour(importedGpx, sport)
+	if err != nil {
+		return err
+	}
+
+	tour = client.updateTourSettings(tour, name, sport)
+
+	return client.createTour(tour)
+}
+
+func (client *Client) importGpx(gpxData string) (*UploadedTour, error) {
 	params := url.Values{}
 	params.Set("data_type", "gpx")
 
@@ -19,38 +35,110 @@ func (client *Client) Upload(name string, gpxData string, sport string) error {
 		fmt.Sprintf("https://www%s/api/routing/import/files/?%s", client.komootDomain, params.Encode()), bytes.NewBuffer([]byte(gpxData)),
 	)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	req.Header.Set("Accept", "application/hal+json,application/json")
 
 	resp, err := client.httpClient.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	var r UploadTourResponse
 	if err := json.Unmarshal(body, &r); err != nil {
-		return err
+		return nil, err
 	}
 
 	if len(r.Embedded.Items) != 1 {
-		return errors.New("import failed: " + r.Message)
+		return nil, errors.New("import failed: " + r.Message)
 	}
 
-	tourJson, err := json.Marshal(r.Embedded.Items[0])
+	return &r.Embedded.Items[0], nil
+}
+
+func (client *Client) importTour(tour *UploadedTour, sport string) (*MatchedTour, error) {
+	tourJson, err := json.Marshal(tour)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(
+		http.MethodPost,
+		fmt.Sprintf("https://www%s/api/routing/import/tour?sport=%s&_embedded=way_types%%2Csurfaces%%2Cdirections%%2Ccoordinates", client.komootDomain, sport),
+		bytes.NewBuffer(tourJson),
+	)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/hal+json,application/json")
+
+	resp, err := client.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var r UploadTourResponse
+	if err := json.Unmarshal(body, &r); err != nil {
+		return nil, err
+	}
+
+	return &r.Embedded.Matched, nil
+}
+
+func (client *Client) updateTourSettings(tour *MatchedTour, name string, sport string) *MatchedTour {
+	tour.Name = name
+	tour.Sport = sport
+	tour.Status = "public"
+	tour.Type = "tour_planned"
+	tour.Constitution = 4
+
+	firstPoint := tour.Path[0]
+	lastPoint := tour.Path[len(tour.Path)-1]
+	firstPoint.Reference = "special:back"
+	firstPoint.Index = lastPoint.Index + 1
+
+	tour.Path = append(tour.Path, firstPoint)
+
+	tour.Segments = append(tour.Segments, Segment{
+		From: lastPoint.Index,
+		To:   lastPoint.Index + 1,
+		Type: "Routed",
+	})
+
+	lastCoordinate := tour.Embedded.Coordinates.Items[len(tour.Embedded.Coordinates.Items)-1]
+
+	tour.Embedded.Coordinates.Items = append(tour.Embedded.Coordinates.Items, Coordinate{
+		Lat: firstPoint.Location.Lat,
+		Lng: firstPoint.Location.Lng,
+		Alt: firstPoint.Location.Alt,
+		T:   lastCoordinate.T + 1,
+	})
+
+	return tour
+}
+
+func (client *Client) createTour(tour *MatchedTour) error {
+	tourJson, err := json.Marshal(tour)
 	if err != nil {
 		return err
 	}
 
-	req, err = http.NewRequest(
+	req, err := http.NewRequest(
 		http.MethodPost,
-		fmt.Sprintf("https://www%s/api/routing/import/tour?sport=%s&_embedded=way_types%%2Csurfaces%%2Cdirections%%2Ccoordinates", client.komootDomain, sport),
+		fmt.Sprintf("https://www%s/api/v007/tours/?hl=nl", client.komootDomain),
 		bytes.NewBuffer(tourJson),
 	)
 	if err != nil {
@@ -59,68 +147,7 @@ func (client *Client) Upload(name string, gpxData string, sport string) error {
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Accept", "application/hal+json,application/json")
 
-	resp, err = client.httpClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	body, err = io.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	var r2 UploadTourResponse
-	if err := json.Unmarshal(body, &r2); err != nil {
-		return err
-	}
-
-	tour2 := &r2.Embedded.Matched
-	tour2.Name = name
-	tour2.Sport = sport
-	tour2.Status = "public"
-	tour2.Type = "tour_planned"
-	tour2.Constitution = 4
-
-	firstPoint := tour2.Path[0]
-	lastPoint := tour2.Path[len(tour2.Path)-1]
-	firstPoint.Reference = "special:back"
-	firstPoint.Index = lastPoint.Index + 1
-
-	tour2.Path = append(tour2.Path, firstPoint)
-
-	tour2.Segments = append(tour2.Segments, Segment{
-		From: lastPoint.Index,
-		To:   lastPoint.Index + 1,
-		Type: "Routed",
-	})
-
-	lastCoordinate := tour2.Embedded.Coordinates.Items[len(tour2.Embedded.Coordinates.Items)-1]
-
-	tour2.Embedded.Coordinates.Items = append(tour2.Embedded.Coordinates.Items, Coordinate{
-		Lat: firstPoint.Location.Lat,
-		Lng: firstPoint.Location.Lng,
-		Alt: firstPoint.Location.Alt,
-		T:   lastCoordinate.T + 1,
-	})
-
-	tourJson2, err := json.Marshal(tour2)
-	if err != nil {
-		return err
-	}
-
-	req, err = http.NewRequest(
-		http.MethodPost,
-		fmt.Sprintf("https://www%s/api/v007/tours/?hl=nl", client.komootDomain),
-		bytes.NewBuffer(tourJson2),
-	)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Accept", "application/hal+json,application/json")
-
-	resp, err = client.httpClient.Do(req)
+	resp, err := client.httpClient.Do(req)
 	if err != nil {
 		return err
 	}
