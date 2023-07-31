819c3ff5b4886fea6832b7be5771f62f1285d9a0
Pieter Claerhout <pieter@yellowduck.be>
2023-07-31 12:10:29 +0200

    Better filtering

--- komoot/tours.go
+++ komoot/tours.go
@@ -6,9 +6,6 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-
-	"github.com/danwakefield/fnmatch"
-	"github.com/pieterclaerhout/go-log"
 )
 
 func (client *Client) Tours(userID int, filter string, tourType string) ([]Tour, []byte, error) {
@@ -17,6 +14,7 @@ func (client *Client) Tours(userID int, filter string, tourType string) ([]Tour,
 	params.Set("limit", "1500")
 	params.Set("type", tourType)
 	params.Set("status", "private")
+	params.Set("name", filter)
 
 	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://www%s/api/v007/users/%d/tours/?%s", client.komootDomain, userID, params.Encode()), nil)
 	if err != nil {
@@ -38,21 +36,6 @@ func (client *Client) Tours(userID int, filter string, tourType string) ([]Tour,
 	var r ToursResponse
 	if err := json.Unmarshal(body, &r); err != nil {
 		return nil, nil, err
-	}
-
-	if filter != "" {
-		log.Warn(filter)
-		filteredTours := []Tour{}
-
-		for _, tour := range r.Embedded.Tours {
-			if !fnmatch.Match(filter, tour.Name, 0) {
-				continue
-			}
-			filteredTours = append(filteredTours, tour)
-		}
-
-		r.Embedded.Tours = filteredTours
-
 	}
 
 	return r.Embedded.Tours, body, nil
